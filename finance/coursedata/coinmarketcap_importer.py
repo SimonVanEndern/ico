a1468d2f20249f0b98edd6520d38d8954b022be1
Simon van Endern <simon.koenigx@gmail.com>
2017-10-12 10:39:53 +0200

    Refactoring coinmarketcap importer done, tests still running

--- finance/coursedata/coinmarketcap_importer.py
+++ finance/coursedata/coinmarketcap_importer.py
@@ -5,66 +5,14 @@ import logging
 import os.path
 import time
 
-from finance.coursedata import exporter
 from global_data import GlobalData
 
 logging.basicConfig(level=logging.DEBUG)
 
-# http.client.HTTPSConnection.debuglevel = 1
 
-# Variables to be used
-times = []
-currencies = []
-tickerSymbols = []
-data = []
+# exporter = exporter.Exporter()
 
-basicUrl = "graphs.coinmarketcap.com"
-folderPath = "Z:\Google Drive\\01 - Studium\Bachelorarbeit\data\coinmarketcap-2017-10-10\\"
-
-# Get currently supported currencies
-conn = http.client.HTTPSConnection("api.coinmarketcap.com")
-conn.request("GET", "/v1/ticker/")
-
-response = conn.getresponse()
-
-currencies = json.loads(response.read().decode("UTF-8"))
-
-# Get ticker symbol of currencies for requests.
-for currency in currencies:
-    tickerSymbols.append(currency["id"])
-
-if tickerSymbols[0] != "bitcoin":
-    print("Error")
-    print(tickerSymbols[0])
-
-exporter = exporter.Exporter()
-
-
-# GetData
-# for idx, symbol in enumerate(tickerSymbols):
-#     # if idx == 10:
-#     #     break
-#
-#     try:
-#         path = folderPath + symbol + ".csv"
-#         if os.path.isfile(path):
-#             print("Already exists: " + path)
-#             continue
-#
-#         conn = http.client.HTTPSConnection(basicUrl)
-#         conn.request("GET", "/currencies/" + symbol + "/")
-#
-#         response = conn.getresponse()
-#         data = json.loads(response.read().decode("UTF-8"))
-#
-#         exporter.export(data, folderPath + symbol + ".csv")
-#
-#         time.sleep(10)
-#
-#     except json.decoder.JSONDecodeError as ex:
-#         print(ex)
-#         print("Exception: " + symbol)
-#         time.sleep(10)
+# ToDo: Automatically reduce received data (timestamp is there for any datapoint)
 
 
 class CoinmarketcapImportFinanceData:
@@ -107,7 +55,7 @@ class CoinmarketcapImportFinanceData:
     def request_data(self, symbol, start, end):
         print("Sleeping for 2 secs")
         time.sleep(2)
-        conn = http.client.HTTPSConnection(basicUrl)
+        conn = http.client.HTTPSConnection(self.basicUrl)
         path = "/currencies/{}/{}/{}/".format(symbol, start, end)
         conn.request("GET", path)
 
