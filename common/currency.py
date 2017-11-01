f886d2a8f50d11296f7b7a6613f607476ff69232
Simon van Endern <simon.koenigx@gmail.com>
2017-11-01 18:21:20 +0100

    Currency refactoring ongoing, deleted not used code, connected to CurrencyStatisticalData

--- common/currency.py
+++ common/currency.py
@@ -9,6 +9,7 @@ import pandas
 from scipy import stats
 
 from common.calculations import calculate_linear_regression
+from common.currency_statistical_data import CurrencyStatisticalData
 from common.time_series import TimeSeries
 from global_data import GlobalData
 
@@ -20,6 +21,8 @@ class Currency:
 
     def __init__(self, currency, data_path=None, date_limit=None):
         self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.info("Initiating currency {}".format(self.currency))
+
         self.date_limit = date_limit
         if self.date_limit is not None:
             self.date_limit = datetime.datetime.strptime(date_limit, "%d.%m.%Y")
@@ -29,7 +32,6 @@ class Currency:
             self.data_path: str = data_path
 
         self.currency: str = currency
-        self.logger.info("Initiating currency {}".format(self.currency))
 
         # Inputs
         self.usd: TimeSeries = None
@@ -39,22 +41,13 @@ class Currency:
 
         self.load_financial_data()
 
-        # Augmentations
         self.start_date: int = self.calculate_start_date()
 
-        # Calculations
-        # self.daily_return: int = None
-        # self.volatility = dict()
         self.price_linear_regression = None
         self.volume_linear_regression = None
         self.volume_return_correlations = None
         self.volume_relative_change = None
 
-        # self.volume_average = None
-        # self.total_volume = 0
-        # self.first_price = 0
-        # self.highest_price = 0
-        # self.lowest_price = 0
         self.maximum_loss = 0
         self.gain_over_total_listing_period = 0
 
@@ -63,28 +56,23 @@ class Currency:
 
         self.instantiate()
 
+        self.statistical_data = CurrencyStatisticalData(self)
+
     def instantiate(self):
-        # self.limit_data()
         self.logger.info("Initiating currency {}".format(self.currency))
 
         self.price_linear_regression = calculate_linear_regression(self.usd)
         self.volume_linear_regression = calculate_linear_regression(self.volume)
 
-        # self.volume_return_corrs = self.calculate_volume_return_correlation()
-
-        # self.highest_market_capitalization = self.calculate_highest_market_capitalization()
         self.maximum_loss = 1 - self.lowest_price / self.highest_price
         if self.usd.data[0] != 0:
             self.gain_over_total_listing_period = self.usd.data[len(self.usd.data) - 1] / self.usd.data[0]
         else:
             self.gain_over_total_listing_period = None
 
-        # self.daily_return = self.calculate_daily_return()
-        # self.volatility = self.calculate_rolling_volatility()
 
     def print(self):
         print("Currency: {} - Gaps: {}".format(self.currency, self.usd.number_of_gaps()))
-        # print("Volume return correlation: {}".format(self.volume_return_corrs))
 
     def load_financial_data(self) -> None:
         filename = self.currency + str(GlobalData.last_date_for_download) + ".csv"
@@ -163,16 +151,6 @@ class Currency:
     def get_volume_financial_data(self):
         return self.volume
 
-    # def calculate_rolling_volatility(self, windows=None) -> dict:
-    #     if windows is None:
-    #         windows = [30, 90, 180]
-    #
-    #     output: dict = dict()
-    #     for window in windows:
-    #         output[str(window)] = pandas.rolling_std(self.usd.relative_change, window)
-    #
-    #     return output
-
     def calculate_linear_regression_on_volatility(self):
         y_values = self.volatility["30"]
         return self.calculate_linear_regresseion(y_values)
@@ -185,21 +163,6 @@ class Currency:
 
         return stats.linregress(x_values, y_values)
 
-    # def calculate_volume_return_correlation(self, smoothing=1):
-    #     val1_pandas: pandas.DataFrame = self.volume.calculate_relative_change_smoothed(smoothing)
-    #     val2_pandas: pandas.DataFrame = self.usd.calculate_relative_change_smoothed(smoothing)
-    #
-    #     joint_df = val1_pandas.join(val2_pandas, lsuffix="first")
-    #
-    #     return joint_df.corr(method="pearson")[joint_df.columns[0]][1]
-
-    # def calculate_volume_return_correlations(self):
-    #     output = []
-    #     for i in [1, 2, 3, 5, 7, 11, 17, 19, 23, 27, 29, 31, 37, 41, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 91]:
-    #         output.append(self.calculate_volume_return_correlation(smoothing=i))
-    #
-    #     return output
-
     def print_volume_return_correlations(self):
         for correlation in self.volume_return_correlations:
             print(correlation)
@@ -209,9 +172,3 @@ class Currency:
 
     def get_beginning_date(self):
         return self.start_date
-
-    # def calculate_highest_market_capitalization(self):
-    #     return max(self.market_cap.data)
-
-    # def calculate_daily_return(self):
-    #     return self.usd.relative_change
